e637db51b4fe840d4791d6d5b7e533dd8fba5d79
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2013-01-11 12:12:31 +0000

    add script for v4

---
+++ release/generate-v4release.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+tar -cvzf samba-ja-docs-$1.tar.gz docs-xml/COPYING docs-xml/README docs-xml/Samba3-* docs-xml/build docs-xml/htmlconv.php docs-xml/manpages docs-xml/output docs-xml/smbdotconf docs-xml/xslt
+tar -cvzf samba-ja-docs-manpages-$1.tar.gz docs-xml/COPYING docs-xml/README docs-xml/output/manpages
